33fa54645802b74f55a28a4e21ceebb4d2242f8d
William Hua <WilliamHua2@my.unt.edu>
2023-10-04 00:37:16 -0400

    Completed webscraper with selenium. Currently can get the bpm of the song closest matching to the searched song.

---
+++ webscraper.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://tunebat.com/Analyzer')
+
+driver.implicitly_wait(0.5)
+
+search_box = driver.find_element(By.TAG_NAME, 'input')
+search_btn = driver.find_element(By.TAG_NAME, 'button')
+
+search_box.send_keys(input('Enter name of song: '))
+search_btn.click()
+
+driver.implicitly_wait(10)
+
+bpm_result = driver.find_elements(By.TAG_NAME, 'p')[2]
+bpm_value = int(bpm_result.text)
+
+print('BPM is', bpm_value)
+
+driver.quit()
