807ca033c6b74610c78f1d4bb3d7dbc120dd389f
William Hua <WilliamHua2@my.unt.edu>
2023-10-07 22:01:29 -0400

    added http request functionality

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import requests
 
 driver = webdriver.Chrome()
 driver.get('https://tunebat.com/Analyzer')
@@ -13,11 +14,13 @@ search_btn = driver.find_element(By.TAG_NAME, 'button')
 search_box.send_keys(input('Enter name of song: '))
 search_btn.click()
 
-driver.implicitly_wait(0.5)
+driver.implicitly_wait(10)
 
-bpm_result = driver.find_elements(By.TAG_NAME, 'p')[2]
-bpm_value = int(bpm_result.text)
+bpm_result = driver.find_elements(By.TAG_NAME, 'p')
+bpm_value = int(bpm_result[2].text)
 
 print('BPM is', bpm_value)
+
+r = requests.post('http://172.19.0.170/led', json={"bpm": bpm_value,})
 
 driver.quit()
